51c51256b43b543236aa64d6a65bd0a6b1d53cdd
xanderyanov <zokrat@yandex.ru>
2021-02-15 10:57:01 +0400

    10-56

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -65,9 +65,9 @@ var isMobile = {
 };
 
 if ($(".map__area").length) {
-  ymaps.ready(init);
+  ymaps.ready(init1);
 
-  function init() {
+  function init1() {
     var myMap = new ymaps.Map("map", {
       center: [51.53106409505919,46.04686637979697],
       zoom: 17,
@@ -84,42 +84,24 @@ if ($(".map__area").length) {
     myGeoObjects[0] = new ymaps.Placemark(
       [51.53106409505919,46.04686637979697],
       {
-        // Зададим содержимое заголовка балуна.
         balloonContentHeader:
           '<div class="baloon__top">Экспертный центр ДЕВАЙС</div>' +
           '<div class="baloon__description">Все виды экспертиз</div>',
-        // Зададим содержимое основной части балуна.
         balloonContentBody:
           '<div class="baloon__content"><img src="assets/img/logo-map.svg" height="47" width="150">' +
           '<a href="tel:+79376397000">8 (8452) 93-30-88</a>',
-        // Зададим содержимое нижней части балуна.
         balloonContentFooter: '<div class="baloon__footer">Саратов, ул. Октябрьская, д.44, офис №6</div>',
         clusterCaption: "Все виды экспертиз",
-        // Зададим содержимое всплывающей подсказки.
         hintContent: '<div class="baloon__hit">Экспертный центр ДЕВАЙС</div>'
       },
       {
-        // Необходимо указать данный тип макета.
+
         iconLayout: "default#image",
         iconImageHref: "assets/img/marker.png",
-        // Размеры метки.
         iconImageSize: [31, 50],
-        // Смещение левого верхнего угла иконки относительно
-        // её «ножки» (точки привязки).
         iconImageOffset: [-15, -50]
       }
     );
-    // myGeoObjects[1] = new ymaps.Placemark([51.551021850477284,46.01745698280331],{
-    //     clusterCaption: 'Саратовремеонт',
-    //     hintContent: 'Саратовремонт!',
-    //     balloonContentBody: 'Саратовремонт, Саратов, Танкистов ул., 37'
-    // },{
-    //     iconLayout: 'default#image',
-    //     iconImageHref: 'assets/img/marker.png',
-    //     iconImageSize: [30, 48],
-    //     iconImageOffset: [-15, -48]
-    // });
-
     var clusterIcons = [
       {
         href: "/images/pointer.png",
@@ -131,25 +113,74 @@ if ($(".map__area").length) {
     var clusterer = new ymaps.Clusterer({
       clusterDisableClickZoom: false,
       clusterOpenBalloonOnClick: false,
-      // Устанавливаем стандартный макет балуна кластера "Карусель".
       clusterBalloonContentLayout: "cluster#balloonCarousel",
-      // Устанавливаем собственный макет.
-      //clusterBalloonItemContentLayout: customItemContentLayout,
-      // Устанавливаем режим открытия балуна.
-      // В данном примере балун никогда не будет открываться в режиме панели.
       clusterBalloonPanelMaxMapArea: 0,
-      // Устанавливаем размеры макета контента балуна (в пикселях).
       clusterBalloonContentLayoutWidth: 300,
       clusterBalloonContentLayoutHeight: 200,
-      // Устанавливаем максимальное количество элементов в нижней панели на одной странице
       clusterBalloonPagerSize: 5,
-      // Настройка внешего вида нижней панели.
-      // Режим marker рекомендуется использовать с небольшим количеством элементов.
       clusterBalloonPagerType: "marker"
-      // Можно отключить зацикливание списка при навигации при помощи боковых стрелок.
-      // clusterBalloonCycling: false,
-      // Можно отключить отображение меню навигации.
-      // clusterBalloonPagerVisible: false
+    });
+
+    clusterer.add(myGeoObjects);
+    myMap.geoObjects.add(clusterer);
+  }
+}
+
+if ($(".map__area").length) {
+  ymaps.ready(init2);
+
+  function init2() {
+    var myMap = new ymaps.Map("map2", {
+      center: [55.70807893624359,37.65165478570555],
+      zoom: 17,
+      controls: ["zoomControl"]
+    });
+
+    myMap.behaviors.disable("scrollZoom");
+    if (isMobile.any()) {
+      myMap.behaviors.disable("drag");
+    }
+
+    var myGeoObjects = [];
+
+    myGeoObjects[0] = new ymaps.Placemark(
+      [55.70807893624359,37.65165478570555],
+      {
+        balloonContentHeader:
+          '<div class="baloon__top">Экспертный центр ДЕВАЙС</div>' +
+          '<div class="baloon__description">Все виды экспертиз</div>',
+        balloonContentBody:
+          '<div class="baloon__content"><img src="assets/img/logo-map.svg" height="47" width="150">' +
+          '<a href="tel:+74993913088">8 (499) 391-30-88</a>',
+        balloonContentFooter: '<div class="baloon__footer">Москва, ул. Ленинская Слобода, 19, 2 этаж, офис №287-1</div>',
+        clusterCaption: "Все виды экспертиз",
+        hintContent: '<div class="baloon__hit">Экспертный центр ДЕВАЙС</div>'
+      },
+      {
+
+        iconLayout: "default#image",
+        iconImageHref: "assets/img/marker.png",
+        iconImageSize: [31, 50],
+        iconImageOffset: [-15, -50]
+      }
+    );
+    var clusterIcons = [
+      {
+        href: "/images/pointer.png",
+        size: [31, 40],
+        offset: [0, 0]
+      }
+    ];
+
+    var clusterer = new ymaps.Clusterer({
+      clusterDisableClickZoom: false,
+      clusterOpenBalloonOnClick: false,
+      clusterBalloonContentLayout: "cluster#balloonCarousel",
+      clusterBalloonPanelMaxMapArea: 0,
+      clusterBalloonContentLayoutWidth: 300,
+      clusterBalloonContentLayoutHeight: 200,
+      clusterBalloonPagerSize: 5,
+      clusterBalloonPagerType: "marker"
     });
 
     clusterer.add(myGeoObjects);
